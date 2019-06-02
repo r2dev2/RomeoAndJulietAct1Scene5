b44bac045bcd198fb6ba21296637a8d9f3f451d5
r2dev2bb8 <rbadhe@Ronak-Badhe.localdomain>
2019-06-01 23:29:09 -0700

    first commit

---
+++ performance.py
@@ -0,0 +1,19 @@
+import vlc
+
+effect_objects = []
+song_names = ["knuckles.mp3", "Cantina.mp3", "Huliet.mp3"]
+
+counter = 1
+for i in song_names:
+    effect_objects.append(vlc.MediaPlayer(i))
+    print(str(counter) + " " + i)
+    counter += 1
+
+success = False
+while not success:
+    effect = effect_objects[int(input("effect number ")) - 1]
+    effect.play()
+    stop = input("close effect(press anything) ")
+    effect.stop()
+    if stop == "quit":
+        success = True

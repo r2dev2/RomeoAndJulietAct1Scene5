55231304ee188009bd1d9e90b30c7669d54aafcc
r2dev2bb8 <50760816+r2dev2bb8@users.noreply.github.com>
2019-06-03 15:42:49 -0700

    Add effect

--- performance.py
+++ performance.py
@@ -1,19 +1,18 @@
 import vlc
 
 effect_objects = []
-song_names = ["knuckles.mp3", "Cantina.mp3", "Huliet.mp3"]
+song_names = ["knuckles.mp3", "Cantina.mp3", "Huliet.mp3", "Snap.mp3"]
 
-counter = 1
-for i in song_names:
-    effect_objects.append(vlc.MediaPlayer(i))
-    print(str(counter) + " " + i[:-4])
-    counter += 1
+for i, song in enumerate(song_names):
+    effect_objects.append(vlc.MediaPlayer(song))
+    print(i, song[:-4])
 
 success = False
 while not success:
     effect = effect_objects[int(input("effect number ")) - 1]
+    effect.audio_set_volume(100)
     effect.play()
     stop = input("close effect(press enter) ")
-    effect.stop()
+    effect.pause()
     if stop == "quit":
         success = True

a4b72e2001d8f145b7f20f9244927ece0ed5a9de
r2dev2bb8 <50760816+r2dev2bb8@users.noreply.github.com>
2019-06-03 15:43:55 -0700

    Add files via upload

---
+++ py2performance.py
@@ -0,0 +1,18 @@
+import vlc
+
+effect_objects = []
+song_names = ["Knuckles.mp3", "Cantina.mp3", "Huliet.mp3", "Snap.mp3"]
+
+for i, song in enumerate(song_names, 1):
+    effect_objects.append(vlc.MediaPlayer(song))
+    print i, song[:-4]
+
+success = False
+while not success:
+    effect = effect_objects[int(input("effect number ")) - 1]
+    effect.audio_set_volume(100)
+    effect.play()
+    stop = input("close effect(press anything) ")
+    effect.pause()
+    if stop == "quit":
+        success = True
